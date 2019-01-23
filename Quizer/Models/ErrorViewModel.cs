24b6a22007a08e6f69fa43392c15cb3e67981bb4
Jacek Noga <jaceknoga11@gmail.com>
2019-01-23 20:29:29 +0100

    Add project files.

---
+++ Quizer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Quizer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
